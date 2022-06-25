5e77a40e756f2a0db4e827f3409314b679ed1a71
Roman Romanenko <romarom81@mail.ru>
2022-06-25 17:44:08 +0300

    Маша

---
+++ Example005_Condition if-else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя:");
+string username = Console.ReadLine ();
+if (username.ToLower () == "маша")
+{
+    Console.Write ("Уррра, это же Маша!");
+}
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (username);
+}
